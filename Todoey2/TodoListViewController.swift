66ebd8544c9a17f79ad8ecef2ac001bdf50824c2
Morgan Hondros <morganhondros@gmail.com>
2019-02-11 12:51:51 -0500

    Beginning of Lecture 224. Created Basic Checkmark Functionality.

---
+++ Todoey2/TodoListViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Todoey2
+//
+//  Created by Morgan Hondros on 2/11/19.
+//  Copyright © 2019 Morgan Hondros. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+    
+    //MARK: - Table View Datasource Methods.
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+
+    //MARK: - Tableview Delegate Methods.
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        print(itemArray.count)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else { tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+
+
+
+        
+    }
+    
+    
+    
+    
+}
+
