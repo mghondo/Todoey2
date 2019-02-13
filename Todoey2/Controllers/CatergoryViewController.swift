5724654b3f033ae4a5f05f1919921b1637df7785
Morgan Hondros <morganhondros@gmail.com>
2019-02-12 20:47:56 -0500

    Before lecture 247: Challenge in next lecture to implement core data into Category View Controller

---
+++ Todoey2/Controllers/CatergoryViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CatergoryViewController.swift
+//  Todoey2
+//
+//  Created by Morgan Hondros on 2/12/19.
+//  Copyright © 2019 Morgan Hondros. All rights reserved.
+//
+
+import UIKit
+
+class CatergoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    
+    //MARK: - TABLEVIEW DATASOURCE METHODS
+    
+    
+    //MARK: - TABLEVIEW DELEGATE METHODS
+    
+    
+    //MARK: - ADD NEW CATEGORIES
+
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+
+    
+    
+    //MARK: - DATA MANIPULATION METHODS
+    
+}
